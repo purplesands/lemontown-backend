7bf7a560b5b417286e1149aa3b809f2b6a6a4d01
purplesands <mullenjoe13@gmail.com>
2019-04-05 00:33:51 -0400

    followed users not working..

--- app/serializers/following_serializer.rb
+++ app/serializers/following_serializer.rb
@@ -1,3 +1,5 @@
 class FollowingSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :followed_user_id, :user
+
+
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :posts
+  attributes :id, :username, :posts, :followed_users
+
 end
