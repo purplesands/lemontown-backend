6e396cf7779069c3abea89c32c6f53c01c923ae5
purplesands <mullenjoe13@gmail.com>
2019-04-26 18:15:57 -0400

    o

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,9 +35,9 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = "wss://lemon-town-api.herokuapp.com/cable"  
+  config.action_cable.url = "ws://localhost:3000/cable"
 
-  config.action_cable.allowed_request_origins = '*'
+  config.action_cable.disable_request_forgery_protection = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
