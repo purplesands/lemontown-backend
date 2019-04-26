85bf00f0795ef776cf83584a7078f7b3a477c740
purplesands <mullenjoe13@gmail.com>
2019-04-26 15:58:23 -0400

    ok

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,7 +1,6 @@
 class AuthController < ApplicationController
   def login
     @user = User.find_by(username: params[:username])
-    puts "hi" @user
     if @user && @user.authenticate(params[:password])
       # prove that the user exists in our database
       # -> lets send them a token!
