d3eac2c092be1d06661f3a36b00bd11e15475a44
purplesands <mullenjoe13@gmail.com>
2019-04-26 14:30:12 -0400

    more

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,6 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :amazon
-  config.require_master_key = true
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
