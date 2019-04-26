3429716dac4fae4f09cc39c24d2a6c6db46b6ae4
purplesands <mullenjoe13@gmail.com>
2019-04-26 14:32:47 -0400

    jwt encryption

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::API
   def encode_token(payload)
-    JWT.encode(payload, ENV['KEY'])
+    JWT.encode(payload, <%= Rails.application.credentials.jw %>)
   end
 
   def decode_token
     begin
-      JWT.decode(auth_headers, ENV['KEY'])
+      JWT.decode(auth_headers, <%= Rails.application.credentials.jw %>)
     rescue
       nil
     end
