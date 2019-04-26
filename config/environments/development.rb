a0805c6a08ccad4f0ee981279f6455259bdbc914
purplesands <mullenjoe13@gmail.com>
2019-04-26 18:33:37 -0400

    umm"

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,6 +46,8 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  config.action_cable.url = "ws://#{ENV['RAILS_HOST']}/cable"
+  config.action_cable.allowed_request_origins = ["https://#{ENV['RAILS_HOST']}", "http://#{ENV['RAILS_HOST']}"]
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
