91f8736a6ff327f7fc24b0c06e8bf19748feeff8
purplesands <mullenjoe13@gmail.com>
2019-04-26 16:12:50 -0400

    action cable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,8 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.action_cable.url = "https://lemon-town-api.herokuapp.com/cable"
+
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
