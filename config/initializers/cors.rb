0d934215da0efdeda74e79a5a9695dcf6bcb6f0f
purplesands <mullenjoe13@gmail.com>
2019-05-17 12:47:09 -0400

    fix cors errors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001'
-    # origins 'lemon-town.herokuapp.com'
+    # origins 'localhost:3001',
+    origins 'lemon-town.herokuapp.com'
 
 
     resource '*',
